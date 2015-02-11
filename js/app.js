6b5dcbb871baf1c4cd9b8daaa0b9faa3420d03c6
Jsteeves10 <jsteevesgit@gmail.com>
2015-02-10 19:29:30 -0500

    append 1 image working

--- js/app.js
+++ js/app.js
@@ -10,25 +10,33 @@ $(document).ready(function(){
 
 	function getRequest(searchTerm){
 	  var params = {
-	  	part: 'snippet'
-	    key: 'AIzaSyDU8SqK32jNb6wdalq_57-ICqfDJKEcL7g'
+	  	part: 'snippet',
+	    key: 'AIzaSyDU8SqK32jNb6wdalq_57-ICqfDJKEcL7g',
 	    q: searchTerm,
 //	    r: 'json'
 	  };
 	  url = 'https://www.googleapis.com/youtube/v3/search';
 
 	  $.getJSON(url, params, function(data){
-	    showResults(data.Search);
+	  	var result = data.items[0].snippet.thumbnails.default.url;
+	  	var image = '<img id="image" src="' +result+ '"/>'
+	    console.log(data);
+	    console.log(result);
+	    console.log(image);
+	    // $.each(results, function(index, value){
+	    // 	image = '<img id="image" src="' +result+ '"/>'
+	    // })
+	    $("#search-results").append(image);
 	  });
 }
 
-	function showResults(results){
-	  var html = "";
-	  $.each(results, function(index,value){
-	    html += '<p>' + value.Title + '</p>';
-	    console.log(value.Title);
-	  });
-	  $('#search-results').html(html);
-	}
+	// function showResults(results){
+	//   var html = "";
+	//   $.each(results, function(index,value){
+	//     html += '<p>' + value.Title + '</p>';
+	//     console.log(value.Title);
+	//   });
+	//   $('#search-results').html(html);
+	// }
 
 });
